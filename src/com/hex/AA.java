c74a546b2710123d49f5ad88b2faa5c988a5c535
Thikazh@18 <rp.vinotha@gmail.com>
2023-10-06 15:01:47 +0530

    welcome

---
+++ src/com/hex/AA.java
@@ -0,0 +1,15 @@
+package com.hex;
+
+public class AA {
+
+	char c;
+	private void fddg() {
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		AA asf=new AA();
+		asf.fddg();
+	}
+	}
+
+
